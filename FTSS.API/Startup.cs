4f1a14cb1c4bb33a02a520ef55f49db3084aaaee
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-01-04 20:53:30 +0330

    Initial project

---
+++ FTSS.API/Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FTSS.API
+{
+    public class Startup
+    {
+        #region properties
+        private const string ConnectionStringName = "cns";
+        private string cns
+        {
+            get
+            {
+                var rst = Configuration.GetConnectionString(ConnectionStringName);
+                return (rst);
+            }
+        }
+
+        public IConfiguration Configuration { get; }
+        #endregion properties
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<Dataprovider.SalamatDBContext>(options => options.UseSqlServer(cns));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ FTSS.Models/Database/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FTSS.Models.Database
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
