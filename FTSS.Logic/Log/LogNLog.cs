712bcc0865b17b78c1e731e1822e269e30074565
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-02-13 20:07:15 +0330

    Add unit test projects

---
+++ FTSS.Logic/Log/LogNLog.cs
@@ -0,0 +1,46 @@
+﻿using NLog;
+using System;
+
+namespace FTSS.Logic.Log
+{
+    public class LogNLog : ILog
+    {
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public LogNLog()
+        {
+        }
+
+        public void Information(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            logger.Warn(message);
+        }
+
+        public void Debug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void Add(Exception e, string customMessage = null)
+        {
+            string text = string.Format("{0}\nException: {1}\nStackTrace: {2}\n",
+                customMessage ?? "", e.Message, e.StackTrace);
+            this.Add(text);
+        }
+
+        public void Add(string message)
+        {
+            Information(message);
+        }
+    }
+}
