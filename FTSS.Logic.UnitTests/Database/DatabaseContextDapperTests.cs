c39117bc96b013bd583d8fa2857f14544a9a11ff
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-02-13 23:30:18 +0330

    Add SP_Log_Insert unit tests

--- FTSS.Logic.UnitTests/Database/DatabaseContextDapperTests.cs
+++ FTSS.Logic.UnitTests/Database/DatabaseContextDapperTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,6 +11,7 @@ namespace FTSS.Logic.UnitTests.Database
     {
         readonly string _connectionString = "Not empty string";
         Logic.Database.IDatabaseContext _dbCTX;
+
         [SetUp]
         public void Setup()
         {
@@ -37,6 +39,7 @@ namespace FTSS.Logic.UnitTests.Database
             Assert.That(result, Is.InstanceOf<Logic.Database.IDatabaseContext>());
         }
 
+        #region SP_Log_Insert
         [Test]
         public void SP_Log_Insert_WhenPassingNullInputs_ThrowsArgumentNullException()
         {
@@ -57,5 +60,36 @@ namespace FTSS.Logic.UnitTests.Database
             Assert.That(() => _dbCTX.SP_Log_Insert(inputs),
                 Throws.ArgumentNullException);
         }
+
+        [Test]
+        public void SP_Log_Insert_WhenPassingValidData_ItReturnDBResult()
+        {
+            var inputs = new Models.Database.StoredProcedures.SP_Log_Insert.Inputs()
+            {
+                MSG = "Simple log message"
+            };
+            var sp = new Mock<Models.Database.ISP<Models.Database.StoredProcedures.SP_Log_Insert.Inputs>>();
+            sp.Setup(s => s.Call(inputs)).Returns(new Models.Database.DBResult());
+
+            var result = _dbCTX.SP_Log_Insert(inputs, sp.Object);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf(typeof(Models.Database.DBResult)));
+        }
+
+        [Test]
+        public void SP_Log_Insert_WhenPassingValidData_ItRunsCallMethod()
+        {
+            var inputs = new Models.Database.StoredProcedures.SP_Log_Insert.Inputs()
+            {
+                MSG = "Simple log message"
+            };
+            var sp = new Mock<Models.Database.ISP<Models.Database.StoredProcedures.SP_Log_Insert.Inputs>>();
+
+            _dbCTX.SP_Log_Insert(inputs, sp.Object);
+
+            sp.Verify(s => s.Call(inputs));
+        }
+        #endregion SP_Log_Insert
     }
 }
