0ffcf17dd82557d00edcbc8d35f853cd27a41f7c
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-02-15 20:06:31 +0330

    Add LogAtDatabase unit tests

---
+++ FTSS.Logic.UnitTests/Log/LogAtDatabaseTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Moq;
+
+namespace FTSS.Logic.UnitTests.Log
+{
+    [TestFixture]
+    class LogAtDatabaseTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        public void 
+    }
+}
