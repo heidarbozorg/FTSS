0af445805a641d695ade6cb02d6fa61cfa493417
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-03-10 13:49:45 +0330

    Add unit tests for API project.

---
+++ FTSS.API.UnitTests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FTSS.API.UnitTests.Controllers
+{
+    [TestFixture]
+    class UsersControllerTests
+    {
+        private API.Controllers.UsersController userController;
+
+        [SetUp]
+        public void Setup()
+        {
+            userController = new API.Controllers.UsersController(dbCTX: null, logger: null, null, null, null);
+        }
+
+        [Test]
+        public void Login_WhenCtxAndLoggerIsNull_ShouldThrowsException()
+        {
+            Assert.That(() => { userController.Login(null); }, Throws.Exception);
+        }
+    }
+}
---
+++ FTSS.DP.DapperORM.UnitTests/StoredProcedure/SP_APILog_InsertTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using Moq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FTSS.DP.DapperORM.UnitTests.StoredProcedure
+{
+    [TestFixture]
+    class SP_APILog_InsertTests
+    {
+        Mock<FTSS.DP.DapperORM.ISQLExecuter> _executer;
+        Models.Database.StoredProcedures.SP_APILog_Insert.Inputs _data;
+        DP.DapperORM.StoredProcedure.SP_APILog_Insert apiObject;
+
+        [SetUp]
+        public void Setup()
+        {
+            _data = new Models.Database.StoredProcedures.SP_APILog_Insert.Inputs();
+            _executer = new Mock<FTSS.DP.DapperORM.ISQLExecuter>();
+            apiObject = new DP.DapperORM.StoredProcedure.SP_APILog_Insert("my connection string", _executer.Object);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        public void Constructor_WhenConnectionStringIsEmpty_ThrowsArgumentNullException(string cns)
+        {
+            Assert.That(() => {
+                new DP.DapperORM.StoredProcedure.SP_APILog_Insert(cns);
+            }, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Constructor_WhenConnectionStringIsValid_NotThrowsArgumentNullException()
+        {
+            var apiObject = new DP.DapperORM.StoredProcedure.SP_APILog_Insert("my connection string");
+            Assert.That(apiObject, Is.Not.Null);
+            Assert.That(apiObject, Is.TypeOf<DP.DapperORM.StoredProcedure.SP_APILog_Insert>());
+        }
+
+        [Test]
+        public void Call_WhenDataIsNull_ThrowsArgumentNullException()
+        {
+            Assert.That(() => { apiObject.Call(null); }, Throws.ArgumentNullException);
+        }
+
+
+        [Test]
+        public void Call_WhenDataIsValid_ReturnDBResult()
+        {            
+            var result = apiObject.Call(_data);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.ErrorCode, Is.EqualTo(200));
+        }
+    }
+}
