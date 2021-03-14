71c75245d562c5afd7c31ace206b41ee2ee8ee1e
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-03-14 10:19:26 +0330

    Extract SP_User_AccessToAPI tests  from DatabaseContextDapperTests to a separate file.

--- FTSS.Logic.UnitTests/Database/DatabaseContextDapperTests.cs
+++ FTSS.Logic.UnitTests/Database/DatabaseContextDapperTests.cs
@@ -8,19 +8,12 @@ namespace FTSS.Logic.UnitTests.Database
     {
         readonly string _connectionString = "Not empty string";
         Logic.Database.IDatabaseContext _dbCTX;
-        Models.Database.StoredProcedures.SP_User_AccessToAPI.Inputs _accessToAPIInputs;
 
 
         [SetUp]
         public void Setup()
         {
             _dbCTX = new Logic.Database.DatabaseContextDapper(_connectionString);
-            
-            _accessToAPIInputs = new Models.Database.StoredProcedures.SP_User_AccessToAPI.Inputs()
-            {
-                Token = "TokenValue",
-                APIAddress = "http://Domain.com/api"
-            };
         }
 
         #region Constractor
@@ -46,60 +39,7 @@ namespace FTSS.Logic.UnitTests.Database
         }
         #endregion Constractor
         
-
         
-
-        #region SP_User_AccessToAPI
-        [Test]
-        public void SP_User_AccessToAPI_WhenPassingNullInputs_ThrowsArgumentNullException()
-        {
-            Assert.That(() => _dbCTX.SP_User_AccessToAPI(null),
-                Throws.ArgumentNullException);
-        }
-
-        [TestCase("", "APIAddress")]
-        [TestCase(null, "APIAddress")]
-        [TestCase("TokenValue", "")]
-        [TestCase("TokenValue", null)]
-        [TestCase("", null)]
-        [TestCase("", "")]
-        [TestCase(null, "")]
-        [TestCase(null, null)]
-        [Test]
-        public void SP_User_AccessToAPI_WhenPassingEmptyAPIAddressOrEmptyToken_ThrowsArgumentNullException(string token, string apiAddress)
-        {
-            var inputs = new Models.Database.StoredProcedures.SP_User_AccessToAPI.Inputs()
-            {
-                Token = token,
-                APIAddress = apiAddress
-            };
-
-            Assert.That(() => _dbCTX.SP_User_AccessToAPI(inputs),
-                Throws.ArgumentNullException);
-        }
-
-        [Test]
-        public void SP_User_AccessToAPI_WhenPassingValidData_ItReturnDBResult()
-        {            
-            var sp = new Mock<Models.Database.ISP<Models.Database.StoredProcedures.SP_User_AccessToAPI.Inputs>>();
-            sp.Setup(s => s.Call(_accessToAPIInputs)).Returns(new Models.Database.DBResult());
-
-            var result = _dbCTX.SP_User_AccessToAPI(_accessToAPIInputs, sp.Object);
-
-            Assert.That(result, Is.Not.Null);
-            Assert.That(result, Is.TypeOf(typeof(Models.Database.DBResult)));
-        }
-
-        [Test]
-        public void SP_User_AccessToAPI_WhenPassingValidData_ItRunsCallMethod()
-        {            
-            var sp = new Mock<Models.Database.ISP<Models.Database.StoredProcedures.SP_User_AccessToAPI.Inputs>>();
-
-            _dbCTX.SP_User_AccessToAPI(_accessToAPIInputs, sp.Object);
-
-            sp.Verify(s => s.Call(_accessToAPIInputs));
-        }
-        #endregion SP_User_AccessToAPI
 
         #region SP_Users_GetAll
         [Test]
