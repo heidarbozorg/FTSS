bf5e5ce51e3d1f7f753c24f0df3ff31c9d037155
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-03-25 21:42:28 +0430

    Refactoring SP_User_SetPassword validation inputs.

--- FTSS.Models/Database/StoredProcedures/SP_User_SetPassword.cs
+++ FTSS.Models/Database/StoredProcedures/SP_User_SetPassword.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace FTSS.Models.Database.StoredProcedures
 {
     public class SP_User_SetPassword
     {
-        public class Inputs : BaseDataModelWithToken, IValidatableObject
+        public class Inputs : BaseDataModelWithToken
         {
             [Required]
             [Range(1, int.MaxValue)]
@@ -14,38 +13,6 @@ namespace FTSS.Models.Database.StoredProcedures
 
             [Required]
             public string Password { get; set; }
-
-            /// <summary>
-            /// Validation results
-            /// </summary>
-            public ICollection<ValidationResult> ValidationResults;
-
-
-            public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-            {
-                var results = new List<ValidationResult>();
-                if (string.IsNullOrEmpty(this.Token))
-                    results.Add(new ValidationResult("Token could not be empty.", new string[] { "Token" }));
-
-                if (string.IsNullOrEmpty(this.Password))
-                    results.Add(new ValidationResult("Password could not be empty.", new string[] { "Password" }));
-
-                if (this.UserId <= 0)
-                    results.Add(new ValidationResult("Invalid UserId.", new string[] { "UserId" }));
-
-                return results;
-            }
-
-            /// <summary>
-            /// Check validations
-            /// </summary>
-            /// <returns></returns>
-            public bool IsValid()
-            {
-                ValidationResults = new List<ValidationResult>();
-                var rst = Validator.TryValidateObject(this, new ValidationContext(this, null, null), ValidationResults, false);
-                return rst;
-            }
         }
 
         public class Outputs : SingleId
