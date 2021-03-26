401a73bed355e8a749bea1585c62a727cc027090
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-03-26 13:31:28 +0430

    Add JWT.cs, Roles.cs, and User.cs in the Logic.Security

---
+++ FTSS.Logic/Security/JWT.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTSS.Logic.Security
+{
+    public class JWT
+    {
+        /// <summary>
+        /// Generate token validation
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="issuer"></param>
+        /// <returns></returns>
+        public static TokenValidationParameters GetTokenValidationParameters(string key, string issuer)
+        {
+            if (string.IsNullOrWhiteSpace(key) || string.IsNullOrWhiteSpace(issuer))
+                throw new ArgumentNullException("key and issuer could not be empty. Check application settings.");
+
+            var rst = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = issuer,
+                ValidAudience = issuer,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+            };
+
+            return rst;
+        }
+
+        /// <summary>
+        /// Get authentication event handler by JWT
+        /// </summary>
+        /// <returns></returns>
+        public static JwtBearerEvents GetJWTEvents()
+        {
+            var rst = new JwtBearerEvents
+            {
+                OnAuthenticationFailed = context =>
+                {
+                    if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                        context.Response.Headers.Add("Token-Expired", "true");
+
+                    return Task.CompletedTask;
+                }
+            };
+
+            return rst;
+        }
+
+
+        /// <summary>
+        /// Validating input params
+        /// </summary>
+        /// <param name="claims"></param>
+        /// <param name="key"></param>
+        /// <param name="issuer"></param>
+        /// <param name="expireDate"></param>
+        private static void Validation(Claim[] claims, string key, string issuer, DateTime expireDate)
+        {
+            if (claims == null || claims.Count() == 0)
+                throw new ArgumentNullException("In GenerateJWT, claims could not be null.");
+
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentNullException("In GenerateJWT, key could not be empty.");
+
+            if (string.IsNullOrWhiteSpace(issuer))
+                throw new ArgumentNullException("In GenerateJWT, issuer could not be empty.");
+
+            if (expireDate < DateTime.Now)
+                throw new ArgumentException("In GenerateJWT, expireDate could not be before the current date.");
+        }
+
+        /// <summary>
+        /// Generate JWT based on the claims and security parameters
+        /// </summary>
+        /// <param name="claims"></param>
+        /// <param name="key"></param>
+        /// <param name="issuer"></param>
+        /// <param name="expireDate"></param>
+        /// <returns></returns>
+        public static string Generate(Claim[] claims, string key, string issuer, DateTime expireDate)
+        {
+            Validation(claims, key, issuer, expireDate);
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken
+            (
+                issuer,     //Issure  
+                issuer,     //Audience
+                claims,
+                expires: expireDate,
+                signingCredentials: credentials
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+    }
+}
---
+++ FTSS.Logic/Security/User.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace FTSS.Logic.Security
+{
+    /// <summary>
+    /// This is a repository for users. All users with different roles will be mapped to this class.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Each JWT will be valid for hours which determines by this constant.
+        /// </summary>
+        private const int TokenValidationHours = 12;
+
+        #region Properties
+        public string DbToken { get; set; }
+
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        /// <summary>
+        /// User role
+        /// </summary>
+        public Roles Role { get; set; }
+
+        /// <summary>
+        /// The JWT token for this user
+        /// </summary>
+        public string JWT { get; set; }
+        #endregion Properties
+
+        #region Private methods        
+        /// <summary>
+        /// Get user information as a Claim array for creating JWT
+        /// </summary>
+        /// <returns></returns>
+        private Claim[] GetClaims()
+        {
+            var rst = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Name, this.Email),
+                new Claim("DbToken", this.DbToken),
+                new Claim("Firstname", this.Firstname ?? ""),
+                new Claim("Lastname", this.Lastname ?? ""),
+                new Claim("role", this.Role.ToString())
+            };
+
+            return rst;
+        }
+        #endregion Private methods
+
+        /// <summary>
+        /// Generate JWT based on the User information
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="issuer"></param>
+        public Models.Database.DBResult GetJWT(string key, string issuer, DateTime? expireDate = null)
+        {
+            //Get user claims
+            var claims = GetClaims();
+
+            //Set token expire date-time
+            if (expireDate == null)
+                expireDate = DateTime.Now.AddHours(TokenValidationHours);
+
+            //Generate and set the JWT
+            this.JWT = Security.JWT.Generate(claims, key, issuer, expireDate.Value);
+
+            //Return result as DBResult object
+            return new Models.Database.DBResult(200, "", this, 1);
+        }
+    }
+}
